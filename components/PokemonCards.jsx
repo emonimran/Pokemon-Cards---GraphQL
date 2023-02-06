3af47a850ad2640a1aa8e27aa99461142ffa358f
Md. Imran Hossain <emonimran118@gmail.com>
2023-02-07 00:55:50 +0600

    responsive slider

--- components/PokemonCards.jsx
+++ components/PokemonCards.jsx
@@ -1,7 +1,147 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+import styles from "../styles/Home.module.css";
 
-function PokemonCards() {
-  return <div>PokemonCards</div>;
+import "swiper/css";
+import "swiper/css/navigation";
+
+function PokemonCards({ pokemonDetails, isSmallScreen }) {
+  return (
+    <div>
+      {!isSmallScreen ? (
+        <div className="grid md:grid-cols-5 sm:grid-cols-4 xs:grid-cols-3 gap-8 md:mx-20 mx-8 mt-10">
+          {Object.values(pokemonDetails).map((pokemon) => (
+            <>
+              <div
+                key={pokemon.id}
+                className={`text-black hover:text-white hover:bg-blue transition duration-300 rounded-lg ${styles.pokemon_card} w-[230px] h-[300px]`}
+              >
+                <div className="relative">
+                  <div className="rounded-lg bg-slate-100 ">
+                    <img
+                      src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+                      alt={pokemon.name}
+                      className="w-52 h-52 hover:scale-110 transition duration-500"
+                    />
+                  </div>
+                  <h1 className="absolute top-3 left-3 text-sm text-black hover:text-black">
+                    #{pokemon.id}
+                  </h1>
+                  <h1 className="text-base capitalize font-medium my-2">
+                    {pokemon.name}
+                  </h1>
+
+                  <div className="flex">
+                    {pokemon.types.map((type) => {
+                      let backgroundColor;
+                      switch (type.type.name) {
+                        case "fire":
+                          backgroundColor = "bg-fire";
+                          break;
+                        case "bug":
+                          backgroundColor = "bg-bug";
+                          break;
+                        case "poison":
+                          backgroundColor = "bg-poison";
+                          break;
+                        case "flying":
+                          backgroundColor = "bg-flying";
+                          break;
+                        case "normal":
+                          backgroundColor = "bg-normal";
+                          break;
+                        default:
+                          backgroundColor = "bg-gray";
+                      }
+                      return (
+                        <h2
+                          key={type.type.name}
+                          className={`px-4 py-1 rounded-xl mr-2 text-white capitalize text-sm ${backgroundColor}`}
+                        >
+                          {type.type.name}
+                        </h2>
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+            </>
+          ))}
+        </div>
+      ) : (
+        <div className="mx-4 mt-14">
+          <Swiper
+            modules={[Navigation]}
+            spaceBetween={5}
+            slidesPerView={1.4}
+            navigation={{ clickable: true }}
+            className="h-80 mx-4"
+          >
+            {Object.values(pokemonDetails).map((pokemon) => (
+              <>
+                <SwiperSlide>
+                  <div
+                    key={pokemon.id}
+                    className={`text-black hover:text-white hover:bg-blue transition duration-300 rounded-lg ${styles.pokemon_card} w-[230px] h-[300px]`}
+                  >
+                    <div className="relative">
+                      <div className="rounded-lg bg-slate-100 ">
+                        <img
+                          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+                          alt={pokemon.name}
+                          className="w-52 h-52 hover:scale-110 transition duration-500"
+                        />
+                      </div>
+                      <h1 className="absolute top-3 left-3 text-sm text-black hover:text-black">
+                        #{pokemon.id}
+                      </h1>
+                      <h1 className="text-base capitalize font-medium my-2">
+                        {pokemon.name}
+                      </h1>
+
+                      <div className="flex">
+                        {pokemon.types.map((type) => {
+                          let backgroundColor;
+                          switch (type.type.name) {
+                            case "fire":
+                              backgroundColor = "bg-fire";
+                              break;
+                            case "bug":
+                              backgroundColor = "bg-bug";
+                              break;
+                            case "poison":
+                              backgroundColor = "bg-poison";
+                              break;
+                            case "flying":
+                              backgroundColor = "bg-flying";
+                              break;
+                            case "normal":
+                              backgroundColor = "bg-normal";
+                              break;
+                            default:
+                              backgroundColor = "bg-gray";
+                          }
+                          return (
+                            <h2
+                              key={type.type.name}
+                              className={`px-4 py-1 rounded-xl mr-2 text-white capitalize text-sm ${backgroundColor}`}
+                            >
+                              {type.type.name}
+                            </h2>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              </>
+            ))}
+          </Swiper>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default PokemonCards;
--- components/PokemonContainer.jsx
+++ components/PokemonContainer.jsx
@@ -1,15 +1,11 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import styles from "../styles/Home.module.css";
 import logo from "../assets/media_assets/Logo.png";
 import Image from "next/image";
 import useMediaQuery from "../components/hooks/useMediaQuery";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper";
-import "swiper/css";
 
-import "swiper/css/navigation";
-
-import { useState, useEffect } from "react";
+import PokemonCards from "./PokemonCards";
 
 const gqlQuery = `
   query pokemons($limit: Int, $offset: Int) {
@@ -127,18 +123,6 @@ function PokemonContainer() {
   const isLargeScreen = useMediaQuery("(min-width: 1060px)");
   const isAboveMediumScreen = useMediaQuery("(min-width: 770px)");
 
-  let slide;
-
-  if (isLargeScreen) {
-    slide = 5;
-  } else if (isSmallScreen) {
-    slide = 2;
-  } else if (isAboveMediumScreen) {
-    slide = 4;
-  } else {
-    slide = 4;
-  }
-
   return (
     <div
       className={`${styles.pokemon_container} w-full md:h-[900px] md:h-[900px] sm:h-[1220px] xs:h-[1480px] h-[550px]`}
@@ -146,137 +130,10 @@ function PokemonContainer() {
       <div className="flex items-center justify-center pt-6 z-40">
         <Image src={logo} alt="logo" />
       </div>
-      {!isSmallScreen ? (
-        <div className="grid md:grid-cols-5 sm:grid-cols-4 xs:grid-cols-3 gap-8 md:mx-20 mx-8 mt-10">
-          {Object.values(pokemonDetails).map((pokemon) => (
-            <>
-              <div
-                key={pokemon.id}
-                className={`text-black hover:text-white hover:bg-blue transition duration-300 rounded-lg ${styles.pokemon_card} w-[230px] h-[300px]`}
-              >
-                <div className="relative">
-                  <div className="rounded-lg bg-slate-100 ">
-                    <img
-                      src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
-                      alt={pokemon.name}
-                      className="w-52 h-52 hover:scale-110 transition duration-500"
-                    />
-                  </div>
-                  <h1 className="absolute top-3 left-3 text-sm text-black hover:text-black">
-                    #{pokemon.id}
-                  </h1>
-                  <h1 className="text-base capitalize font-medium my-2">
-                    {pokemon.name}
-                  </h1>
-
-                  <div className="flex">
-                    {pokemon.types.map((type) => {
-                      let backgroundColor;
-                      switch (type.type.name) {
-                        case "fire":
-                          backgroundColor = "bg-fire";
-                          break;
-                        case "bug":
-                          backgroundColor = "bg-bug";
-                          break;
-                        case "poison":
-                          backgroundColor = "bg-poison";
-                          break;
-                        case "flying":
-                          backgroundColor = "bg-flying";
-                          break;
-                        case "normal":
-                          backgroundColor = "bg-normal";
-                          break;
-                        default:
-                          backgroundColor = "bg-gray";
-                      }
-                      return (
-                        <h2
-                          key={type.type.name}
-                          className={`px-4 py-1 rounded-xl mr-2 text-white capitalize text-sm ${backgroundColor}`}
-                        >
-                          {type.type.name}
-                        </h2>
-                      );
-                    })}
-                  </div>
-                </div>
-              </div>
-            </>
-          ))}
-        </div>
-      ) : (
-        <div className="mx-4 mt-14">
-          <Swiper
-            modules={[Navigation]}
-            spaceBetween={5}
-            slidesPerView={1.4}
-            navigation={{ clickable: true }}
-            className="h-80 mx-4"
-          >
-            {Object.values(pokemonDetails).map((pokemon) => (
-              <>
-                <SwiperSlide>
-                  <div
-                    key={pokemon.id}
-                    className={`text-black hover:text-white hover:bg-blue transition duration-300 rounded-lg ${styles.pokemon_card} w-[230px] h-[300px]`}
-                  >
-                    <div className="relative">
-                      <div className="rounded-lg bg-slate-100 ">
-                        <img
-                          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
-                          alt={pokemon.name}
-                          className="w-52 h-52 hover:scale-110 transition duration-500"
-                        />
-                      </div>
-                      <h1 className="absolute top-3 left-3 text-sm text-black hover:text-black">
-                        #{pokemon.id}
-                      </h1>
-                      <h1 className="text-base capitalize font-medium my-2">
-                        {pokemon.name}
-                      </h1>
-
-                      <div className="flex">
-                        {pokemon.types.map((type) => {
-                          let backgroundColor;
-                          switch (type.type.name) {
-                            case "fire":
-                              backgroundColor = "bg-fire";
-                              break;
-                            case "bug":
-                              backgroundColor = "bg-bug";
-                              break;
-                            case "poison":
-                              backgroundColor = "bg-poison";
-                              break;
-                            case "flying":
-                              backgroundColor = "bg-flying";
-                              break;
-                            case "normal":
-                              backgroundColor = "bg-normal";
-                              break;
-                            default:
-                              backgroundColor = "bg-gray";
-                          }
-                          return (
-                            <h2
-                              key={type.type.name}
-                              className={`px-4 py-1 rounded-xl mr-2 text-white capitalize text-sm ${backgroundColor}`}
-                            >
-                              {type.type.name}
-                            </h2>
-                          );
-                        })}
-                      </div>
-                    </div>
-                  </div>
-                </SwiperSlide>
-              </>
-            ))}
-          </Swiper>
-        </div>
-      )}
+      <PokemonCards
+        pokemonDetails={pokemonDetails}
+        isSmallScreen={isSmallScreen}
+      />
     </div>
   );
 }
