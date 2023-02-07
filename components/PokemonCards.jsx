bf1a689d080e453a051a7190e8f511bde381f9b0
Md. Imran Hossain <emonimran118@gmail.com>
2023-02-07 21:28:37 +0600

    Link corrected and icon & title added

--- components/PokemonCards.jsx
+++ components/PokemonCards.jsx
@@ -79,63 +79,65 @@ function PokemonCards({ pokemonDetails, isSmallScreen }) {
             className="h-80 mx-4"
           >
             {Object.values(pokemonDetails).map((pokemon) => (
-              <Link href={`/pokemons/${pokemon.name}`} key={pokemon.id}>
+              <>
                 <SwiperSlide>
-                  <div
-                    key={pokemon.id}
-                    className={`text-black hover:text-white hover:bg-blue transition duration-300 rounded-lg ${styles.pokemon_card} w-[230px] h-[300px]`}
-                  >
-                    <div className="relative">
-                      <div className="rounded-lg bg-slate-100 ">
-                        <img
-                          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
-                          alt={pokemon.name}
-                          className="w-52 h-52 hover:scale-110 transition duration-500"
-                        />
-                      </div>
-                      <h1 className="absolute top-3 left-3 text-sm text-black hover:text-black">
-                        #{pokemon.id}
-                      </h1>
-                      <h1 className="text-base capitalize font-medium my-2">
-                        {pokemon.name}
-                      </h1>
+                  <Link href={`/pokemons/${pokemon.name}`} key={pokemon.id}>
+                    <div
+                      key={pokemon.id}
+                      className={`text-black hover:text-white hover:bg-blue transition duration-300 rounded-lg ${styles.pokemon_card} w-[230px] h-[300px]`}
+                    >
+                      <div className="relative">
+                        <div className="rounded-lg bg-slate-100 ">
+                          <img
+                            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+                            alt={pokemon.name}
+                            className="w-52 h-52 hover:scale-110 transition duration-500"
+                          />
+                        </div>
+                        <h1 className="absolute top-3 left-3 text-sm text-black hover:text-black">
+                          #{pokemon.id}
+                        </h1>
+                        <h1 className="text-base capitalize font-medium my-2">
+                          {pokemon.name}
+                        </h1>
 
-                      <div className="flex">
-                        {pokemon.types.map((type) => {
-                          let backgroundColor;
-                          switch (type.type.name) {
-                            case "fire":
-                              backgroundColor = "bg-fire";
-                              break;
-                            case "bug":
-                              backgroundColor = "bg-bug";
-                              break;
-                            case "poison":
-                              backgroundColor = "bg-poison";
-                              break;
-                            case "flying":
-                              backgroundColor = "bg-flying";
-                              break;
-                            case "normal":
-                              backgroundColor = "bg-normal";
-                              break;
-                            default:
-                              backgroundColor = "bg-gray";
-                          }
-                          return (
-                            <h2
-                              key={type.type.name}
-                              className={`px-4 py-1 rounded-xl mr-2 text-white capitalize text-sm ${backgroundColor}`}
-                            >
-                              {type.type.name}
-                            </h2>
-                          );
-                        })}
+                        <div className="flex">
+                          {pokemon.types.map((type) => {
+                            let backgroundColor;
+                            switch (type.type.name) {
+                              case "fire":
+                                backgroundColor = "bg-fire";
+                                break;
+                              case "bug":
+                                backgroundColor = "bg-bug";
+                                break;
+                              case "poison":
+                                backgroundColor = "bg-poison";
+                                break;
+                              case "flying":
+                                backgroundColor = "bg-flying";
+                                break;
+                              case "normal":
+                                backgroundColor = "bg-normal";
+                                break;
+                              default:
+                                backgroundColor = "bg-gray";
+                            }
+                            return (
+                              <h2
+                                key={type.type.name}
+                                className={`px-4 py-1 rounded-xl mr-2 text-white capitalize text-sm ${backgroundColor}`}
+                              >
+                                {type.type.name}
+                              </h2>
+                            );
+                          })}
+                        </div>
                       </div>
                     </div>
-                  </div>
+                  </Link>
                 </SwiperSlide>
-              </Link>
+              </>
             ))}
           </Swiper>
         </div>
--- pages/pokemons/[pokemonName].jsx
+++ pages/pokemons/[pokemonName].jsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import styles from "../../styles/Home.module.css";
 import logo from "../../assets/media_assets/Logo.png";
 import Link from "next/link";
+import Head from "next/head";
 
 const gqlQuery = `query pokemon($name: String!) {
   pokemon(name: $name) {
@@ -61,215 +62,223 @@ function Pokemons() {
   }, [pokemonDetails]);
 
   return (
-    <div className="xs:flex w-[100vw]">
-      {" "}
-      {!isSmallScreen ? (
-        <Image src={Sidebar} alt="sidebar" className="w-[6%]"></Image>
-      ) : (
-        ""
-      )}
-      <div className="bg-white xs:w-[88%] w-[100%] sm:py-16 py-10">
-        {/* Heading */}
-
-        <div className="flex items-center justify-center">
-          <Image src={logo} alt="logo" />
-        </div>
-
-        {pokemonData.pokemon ? (
-          <div className="mx-8 sm:flex sm:justify-center sm:mt-14">
-            {/* Image Section */}
-            <div className="sm:order-2 sm:w-1/3 flex items-center justify-center">
-              <div className="">
-                <img
-                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonData.pokemon.id}.png`}
-                  alt={pokemonData.pokemon.name}
-                  width={300}
-                  height={300}
-                  className="rounded-lg hover:scale-125 transition duration-500"
-                />
-                {!isMediumScreen ? (
-                  <div className="w-full flex items-center justify-center">
-                    <Link href="/">
-                      <button className="bg-yellow flex text-white border-4 border-blue py-4 px-8 sm:mt-36 font-semibold">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="w-6 h-6 my-auto mr-2"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-                          />
-                        </svg>
-                        Back to Homepage
-                      </button>
-                    </Link>
-                  </div>
-                ) : (
-                  ""
-                )}
-              </div>
-            </div>
-
-            <div className="sm:order-1 sm:w-1/3 sm:mt-0 mt-14">
-              <h1 className="capitalize text-blue font-medium text-4xl mb-4">
-                {pokemonData.pokemon.name} #{pokemonData.pokemon.id}
-              </h1>
-              <h3>
-                There is a plant seed on its back right from the day this
-                Pokémon is born. The seed slowly grows larger.
-              </h3>
-              <div className="sm:block flex items-center justify-center">
-                <div
-                  className={`mt-10 text-black rounded-lg ${styles.stats_outside}`}
-                >
-                  <div
-                    className={`rounded-lg ${styles.stats_inside} flex items-center justify-evenly`}
-                  >
-                    <div className={`grid grid-cols-2 gap-6`}>
-                      <div>
-                        <h1 className="font-medium text-xl">Height</h1>
-                        <p>{pokemonData.pokemon.height}`</p>
-                      </div>
-
-                      <div>
-                        <h1 className="font-medium text-xl">Weight</h1>
-                        <p>{pokemonData.pokemon.weight} lbs</p>
-                      </div>
-
-                      <div>
-                        <h1 className="font-medium text-xl">Abilities</h1>
-                        <ul>
-                          {pokemonData.pokemon.abilities.map((ability) => (
-                            <li key={ability.ability.name}>
-                              -{ability.ability.name}
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
-
-                      <div>
-                        <h1 className="font-medium text-xl">Types</h1>
-                        <ul>
-                          {pokemonData.pokemon.types.map((type) => (
-                            <li key={type.type.name}>-{type.type.name}</li>
-                          ))}
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="sm:order-3 sm:w-1/3 sm:mt-0 mt-14">
-              <div>
-                <h1 className="font-medium text-xl mb-4">Type</h1>
-                <div className="flex items-center justify-start">
-                  {pokemonData.pokemon.types.map((type) => {
-                    let backgroundColor;
-                    switch (type.type.name) {
-                      case "fire":
-                        backgroundColor = "bg-fire";
-                        break;
-                      case "bug":
-                        backgroundColor = "bg-bug";
-                        break;
-                      case "poison":
-                        backgroundColor = "bg-poison";
-                        break;
-                      case "flying":
-                        backgroundColor = "bg-flying";
-                        break;
-                      case "normal":
-                        backgroundColor = "bg-normal";
-                        break;
-                      default:
-                        backgroundColor = "bg-gray";
-                    }
-                    return (
-                      <h2
-                        key={type.type.name}
-                        className={`px-4 py-1 rounded-xl mr-2 text-white capitalize text-sm ${backgroundColor}`}
-                      >
-                        {type.type.name}
-                      </h2>
-                    );
-                  })}
-                </div>
-              </div>
-              <div className="mt-8">
-                <h1 className="font-medium text-xl mb-4">Stats</h1>
-
-                <ul>
-                  {pokemonData.pokemon.stats.map((stat) => (
-                    <div key={stat.stat.name} className="mb-4">
-                      <h1 className="font-medium text-base uppercase mb-2">
-                        {stat.stat.name}:
-                      </h1>
-
-                      <div className="w-full bg-gray-200 rounded-full h-2.5">
-                        <div
-                          className="bg-blue h-2.5 rounded-full"
-                          style={{ width: `${stat.base_stat}%` }}
-                        ></div>
-                      </div>
-                    </div>
-                  ))}
-                </ul>
-              </div>
-            </div>
-            {isMediumScreen ? (
-              <div className="w-full mt-10 flex items-center justify-center">
-                <Link href="/">
-                  <button className="bg-yellow flex text-white border-4 border-blue py-4 px-8 sm:mt-36 font-semibold">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6 my-auto mr-2"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-                      />
-                    </svg>
-                    Back to Homepage
-                  </button>
-                </Link>
-              </div>
-            ) : (
-              ""
-            )}
-          </div>
+    <>
+      <Head>
+        <title>Pokemon Details</title>
+        <meta name="description" content="Generated by create next app" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="xs:flex w-[100vw]">
+        {" "}
+        {!isSmallScreen ? (
+          <Image src={Sidebar} alt="sidebar" className="w-[6%]"></Image>
         ) : (
-          <>
-            <div className="flex items-center justify-center space-x-2 mt-52">
-              <div
-                className="spinner-border animate-spin inline-block w-20 h-20 border-4 rounded-full"
-                role="status"
-              ></div>
-            </div>
+          ""
+        )}
+        <div className="bg-white xs:w-[88%] w-[100%] sm:py-16 py-10">
+          {/* Heading */}
 
-            <h1 className="font-semibold text-2xl text-center mt-4">
-              ...Loading
-            </h1>
-          </>
+          <div className="flex items-center justify-center">
+            <Image src={logo} alt="logo" />
+          </div>
+
+          {pokemonData.pokemon ? (
+            <div className="mx-8 sm:flex sm:justify-center sm:mt-14">
+              {/* Image Section */}
+              <div className="sm:order-2 sm:w-1/3 flex items-center justify-center">
+                <div className="">
+                  <img
+                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonData.pokemon.id}.png`}
+                    alt={pokemonData.pokemon.name}
+                    width={300}
+                    height={300}
+                    className="rounded-lg hover:scale-125 transition duration-500"
+                  />
+                  {!isMediumScreen ? (
+                    <div className="w-full flex items-center justify-center">
+                      <Link href="/">
+                        <button className="bg-yellow flex text-white border-4 border-blue py-4 px-8 sm:mt-36 font-semibold">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6 my-auto mr-2"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                            />
+                          </svg>
+                          Back to Homepage
+                        </button>
+                      </Link>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                </div>
+              </div>
+
+              <div className="sm:order-1 sm:w-1/3 sm:mt-0 mt-14">
+                <h1 className="capitalize text-blue font-medium text-4xl mb-4">
+                  {pokemonData.pokemon.name} #{pokemonData.pokemon.id}
+                </h1>
+                <h3>
+                  There is a plant seed on its back right from the day this
+                  Pokémon is born. The seed slowly grows larger.
+                </h3>
+                <div className="sm:block flex items-center justify-center">
+                  <div
+                    className={`mt-10 text-black rounded-lg ${styles.stats_outside}`}
+                  >
+                    <div
+                      className={`rounded-lg ${styles.stats_inside} flex items-center justify-evenly`}
+                    >
+                      <div className={`grid grid-cols-2 gap-6`}>
+                        <div>
+                          <h1 className="font-medium text-xl">Height</h1>
+                          <p>{pokemonData.pokemon.height}`</p>
+                        </div>
+
+                        <div>
+                          <h1 className="font-medium text-xl">Weight</h1>
+                          <p>{pokemonData.pokemon.weight} lbs</p>
+                        </div>
+
+                        <div>
+                          <h1 className="font-medium text-xl">Abilities</h1>
+                          <ul>
+                            {pokemonData.pokemon.abilities.map((ability) => (
+                              <li key={ability.ability.name}>
+                                -{ability.ability.name}
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+
+                        <div>
+                          <h1 className="font-medium text-xl">Types</h1>
+                          <ul>
+                            {pokemonData.pokemon.types.map((type) => (
+                              <li key={type.type.name}>-{type.type.name}</li>
+                            ))}
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="sm:order-3 sm:w-1/3 sm:mt-0 mt-14">
+                <div>
+                  <h1 className="font-medium text-xl mb-4">Type</h1>
+                  <div className="flex items-center justify-start">
+                    {pokemonData.pokemon.types.map((type) => {
+                      let backgroundColor;
+                      switch (type.type.name) {
+                        case "fire":
+                          backgroundColor = "bg-fire";
+                          break;
+                        case "bug":
+                          backgroundColor = "bg-bug";
+                          break;
+                        case "poison":
+                          backgroundColor = "bg-poison";
+                          break;
+                        case "flying":
+                          backgroundColor = "bg-flying";
+                          break;
+                        case "normal":
+                          backgroundColor = "bg-normal";
+                          break;
+                        default:
+                          backgroundColor = "bg-gray";
+                      }
+                      return (
+                        <h2
+                          key={type.type.name}
+                          className={`px-4 py-1 rounded-xl mr-2 text-white capitalize text-sm ${backgroundColor}`}
+                        >
+                          {type.type.name}
+                        </h2>
+                      );
+                    })}
+                  </div>
+                </div>
+                <div className="mt-8">
+                  <h1 className="font-medium text-xl mb-4">Stats</h1>
+
+                  <ul>
+                    {pokemonData.pokemon.stats.map((stat) => (
+                      <div key={stat.stat.name} className="mb-4">
+                        <h1 className="font-medium text-base uppercase mb-2">
+                          {stat.stat.name}:
+                        </h1>
+
+                        <div className="w-full bg-gray-200 rounded-full h-2.5">
+                          <div
+                            className="bg-blue h-2.5 rounded-full"
+                            style={{ width: `${stat.base_stat}%` }}
+                          ></div>
+                        </div>
+                      </div>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+              {isMediumScreen ? (
+                <div className="w-full mt-10 flex items-center justify-center">
+                  <Link href="/">
+                    <button className="bg-yellow flex text-white border-4 border-blue py-4 px-8 sm:mt-36 font-semibold">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6 my-auto mr-2"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                        />
+                      </svg>
+                      Back to Homepage
+                    </button>
+                  </Link>
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
+          ) : (
+            <>
+              <div className="flex items-center justify-center space-x-2 mt-52">
+                <div
+                  className="spinner-border animate-spin inline-block w-20 h-20 border-4 rounded-full"
+                  role="status"
+                ></div>
+              </div>
+
+              <h1 className="font-semibold text-2xl text-center mt-4">
+                ...Loading
+              </h1>
+            </>
+          )}
+        </div>
+        {!isSmallScreen ? (
+          <Image src={Sidebar} alt="sidebar" className="w-[6%]"></Image>
+        ) : (
+          ""
         )}
       </div>
-      {!isSmallScreen ? (
-        <Image src={Sidebar} alt="sidebar" className="w-[6%]"></Image>
-      ) : (
-        ""
-      )}
-    </div>
+    </>
   );
 }
 
